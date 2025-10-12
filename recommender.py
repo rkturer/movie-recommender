56f7a7ee34280aa4eec0404ca8d501e54866814b
rkturer <rkt26@icloud.com>
2025-10-12 15:39:29 -0400

    I added an 'where_to_watch.py' file that uses an API to find where you can watch the movie returned from the recommender in the US. I also did some bad input handling in the main of the recommender.py file

--- recommender.py
+++ recommender.py
@@ -1,18 +1,42 @@
 
 import pandas as pd 
 
-from user_input import user_input, user_input_options, user_input_genre
+from user_input import user_input
+
+from where_to_watch import convert_title_to_movie_id, get_streaming_options
 
 def main():  
     
     movies = pd.read_csv("data/cleaned_movies.csv")
-    print(movies.head())
     while True:
         genre = str(user_input(movies))
-
+        
         recommendation = find_best_rating(movies, genre)
 
+        if recommendation == "No movies from our database match this genre":
+            print(recommendation + "\nOr your spelling was incorrect")
+            reset = str(input("Would you like to search for another recommendation? (yes/no) "))
+            if reset == "yes": 
+                continue
+            else:
+                print("Thank you for trying the movie-recommender program!")
+                break
+
         display(recommendation)
+        
+        title = title_only(recommendation)
+
+        wants_stream = str(input("Would you like to know where you can stream " + title + "? (yes/no) ")).lower()
+        
+        if wants_stream == "yes":
+
+            movie_id = convert_title_to_movie_id(title)
+
+            streaming, purchase, buy = get_streaming_options(movie_id, title)
+
+            print(display_watch_options(streaming, purchase, buy, title))
+
+        
 
         another = str(input("Would you like another recommendation? (yes/no) "))
         if another.lower() != "yes":
@@ -39,7 +63,7 @@ def find_best_rating(movies, genre):
 
     #handles genres not included within my original data frame
     if df_genre.empty:
-        return "No movies from out database match this genre"
+        return "No movies from our database match this genre"
 
     #find the highest rated movie in the specified genre
     highest_rated = df_genre.loc[df_genre['Rating'].idxmax()]
@@ -55,7 +79,47 @@ def formatter(title, rating):
 def display(output):
     """displays the output to the user"""
     print(output)
-    
 
+def display_watch_options(streaming, renting, purchase, title):
+
+    """takes the streaming options, renting options, purchasing options, and the title as parameters and 
+    returns a nicely formatted output of where the movie is avaliable for the user to read"""
+
+    #boolean checkers to return a different message API has no information at all for how to watch the title
+    x = False
+    y = False
+    z = False
+
+    #if else logic checks to see if there are options for each category: streaming, renting, purchasing  
+    if type(streaming) == list: 
+        streaming_string = "You can stream " + title + " on " + ', '.join(streaming)
+    else:
+        #changes boolean checker to true if no option is avaible
+        x = True 
+        streaming_string = streaming
+    if type(renting) == list: 
+        renting_string = "You can rent " + title + " on " + ', '.join(renting)
+    else:
+        y = True 
+        renting_string = renting 
+
+    if type(purchase) == list: 
+        purchase_string = "You can purchase " + title + " on " + ', '.join(purchase)
+    else:
+        z = True
+        purchase_string = purchase
+
+    #will be make sense once i add a new function to ensure one of the ratings are us based
+    if x and y and z == True:
+        output = "In the United States there is no where to rent, stream, or buy " + title + "according to the Movie Data Base API" 
+        return output #,america_rec() --> will search until it finds the highest rated movie with an american option in the API
+
+    return streaming_string + "\n" + renting_string + "\n" + purchase_string 
+
+def title_only(recommendation):
+    """takes the result of find best rating function and extracts the title"""
+    title = recommendation.split(" (")[0].replace("You should watch ", "")
+    return title
+    
 if __name__ == "__main__":
     main()
---
+++ where_to_watch.py
@@ -0,0 +1,77 @@
+
+from dotenv import load_dotenv 
+import os 
+import requests
+
+load_dotenv()
+
+api_key = os.getenv("TMDB_API_KEY")
+
+def convert_title_to_movie_id(highest_rated_title):
+    """takes the highest rated title as parameter and returns the corresponding 
+    movie id within the movie data base api """
+
+    #uses the url for later use 
+    url = "https://api.themoviedb.org/3/search/movie"
+
+    #sets my api key and uses query to request data about the movie from the parameter 
+    params = {
+        "api_key": api_key,
+        "query": highest_rated_title}
+    
+    #requests data from the api server using the url, my api-key, and the movie title 
+    response = requests.get(url, params=params)
+
+    #converts the data into dictionary
+    movie_data = response.json()
+
+    #extracts the first indexed element in results (normally most relevant one)
+    results = movie_data["results"][0]
+
+    #takes care of senario of the movie not being in the data base 
+    if results == None:
+        return "Your movie was not in The Movie Data Base API"
+    
+    #finds and returns the numerical id corresponding to the movies title 
+    movie_id_tmdb = results['id']
+    return movie_id_tmdb
+    
+
+def get_streaming_options(movie_id_tmdb, highest_rated_title):
+
+    """parameter is the movie id found in the "convert_title_to_movieid" fucntion
+    and uses it to search the api for where the movie can be streamed, bought, or rented """
+    
+    #dyanmically create a API request using formatting string where the parameter "movie_id_tmdb" is added the the url
+    url = f"https://api.themoviedb.org/3/movie/{movie_id_tmdb}/watch/providers"
+    
+    #stores api key as a parameter 
+    params = {"api_key": api_key,}
+
+    #requests the api for the information using the dynamic url and my api key 
+    response = requests.get(url, params=params)
+
+    #converts raw data into a dictionary
+    movie_provider = response.json()
+
+    #filters the data down to only United States 
+    us_results = movie_provider.get("results", {}).get("US", {})
+
+    #handles if no us streaming, renting, or purchasing services have the movie 
+    if us_results == {}:
+        streaming_providers = "On the Movie Data Base API, no streaming services in the US have, " + highest_rated_title
+        renting_options = "On the Movie Data Base API, no renting services in the US have, " + highest_rated_title
+        purchase_options = "On the Movie Data Base API, there are no sites to purchase " + highest_rated_title + " in the US"
+    
+    #uses list comprehension to extract the provider names from the lists of dictionaries 
+    else:
+        streaming_providers = [my_dict["provider_name"] for my_dict in us_results.get("flatrate", [])] or "No streaming options avaliable on Movie Data Base API"
+        renting_options = [my_dict["provider_name"] for my_dict in us_results.get("rent", [])] or "No renting options avaliable on Movie Data Base API"
+        purchase_options = [my_dict["provider_name"] for my_dict in us_results.get("buy", [])] or "No purchasing options avaliable on Movie Data Base API"
+
+    #returns 3 lists of providers corresponding to each option 
+    return streaming_providers, renting_options, purchase_options
+
+
+
+    
